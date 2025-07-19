f22d13c79b4de8deb108f735e71eb0bbcd079986
AaronSchroederMi <Aaron-schroeder-mi@web.de>
2025-07-19 20:47:30 +0200

    Make Question Class to work on
    to use when the JSON file is read in

---
+++ src/main/java/org/Main/Question.java
@@ -0,0 +1,58 @@
+package org.Main;
+
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDate;
+import java.util.List;
+import java.util.Map;
+
+public class Question {
+    private int id;
+    private String question;
+    private Map<String, String> answers;
+    private String correctAnswer;
+    private List<Log> logs;
+    private LocalDateTime thisSession = LocalDateTime.now();
+
+
+    public List<Log> getLogOfThisSession() {
+        return logs.stream()
+                .filter(localTimeDate -> localTimeDate.getTimestamp().isAfter(thisSession))
+                .toList();
+    }
+    public List<Log> getLogOfToday() {
+        return logs.stream()
+                .filter(localTimeDate -> localTimeDate.getTimestamp().toLocalDate().isEqual(ChronoLocalDate.from(thisSession)))
+                .toList();
+    }
+    public int getAskedCount() {
+        return logs.size();
+    }
+    public int getCorrectAnswerCount() {
+        return (int) logs.stream()
+                .filter(Log::isCorrect)
+                .count();
+    }
+    public double getConfidence() {
+        double correctAnswerCount = getCorrectAnswerCount();
+        double askedCount = getAskedCount();
+        return askedCount / correctAnswerCount;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+    public List<Log> getLogs() {
+        return logs;
+    }
+
+    public Map<String, String> getAnswers() {
+        return answers;
+    }
+}
