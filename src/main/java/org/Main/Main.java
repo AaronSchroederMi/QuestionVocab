1b01d5180140215b31a2522bca4bfc498d0cc6da
AaronSchroederMi <Aaron-schroeder-mi@web.de>
2025-07-19 12:43:35 +0200

    make apk window

--- src/main/java/org/Main/Main.java
+++ src/main/java/org/Main/Main.java
@@ -1,5 +1,23 @@
 package org.Main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 400, 300);
+
+        stage.setTitle("VocabQuiz");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
